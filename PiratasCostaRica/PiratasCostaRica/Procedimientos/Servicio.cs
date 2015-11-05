94a05ae24b88a38f976e048392ac74e3e37fd810
Jankcr <jankbrenes94@hotmail.com>
2015-11-05 16:59:10 -0600

    Avance modulo servicios
    agregue las funciones para cuando se aprueba un nuevo servicio de
    transporte, ademas modifique la clase Myconnection que estaba dando unos
    problemas

---
+++ PiratasCostaRica/PiratasCostaRica/Procedimientos/Servicio.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace PiratasCostaRica.Procedimientos
+{
+    public abstract class Servicio
+    {
+        private string ced;
+        private string cedchofer;
+        private string ruta;
+        private double dinero;
+
+        public string Ced { get { return ced; } set { ced = value; } }
+        public string Ruta { get { return ruta; } set { ruta = value; } }
+        public double Dinero { get { return dinero; } set { dinero = value; } }
+        public string Cedchofer { get { return cedchofer; } set { cedchofer = value; } }
+
+        public abstract void SolicitarServicio() { }
+    }
+}
---
+++ PiratasCostaRica/PiratasCostaRica/Procedimientos/ServicioControl.cs
@@ -0,0 +1,42 @@
+﻿using PiratasCostaRica.BDConection;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PiratasCostaRica.Procedimientos
+{
+    class ServicioControl:Servicio
+    {
+        public override bool SolicitarServicio(Servicio serv)
+        {
+
+            bool result = false;
+            try
+            {
+                Myconnection myConnection = new Myconnection();
+                SqlConnection conexion = myConnection.CreateConnection();
+                SqlCommand comando = myConnection.CreateCommand(conexion);
+
+                comando.CommandText = "Recaudacion";
+                comando.CommandType = CommandType.StoredProcedure;
+                comando.Parameters.AddWithValue("@pDinero", serv.Dinero);
+                comando.Parameters.AddWithValue("@pRuta", serv.Ruta);
+                comando.Parameters.AddWithValue("@pCedulaChofer", serv.Cedchofer);
+
+                conexion.Open();
+                comando.ExecuteNonQuery();
+                conexion.Close();
+                result = true;
+            }
+            catch (SqlException e)
+            {
+                result = false;
+            }
+            return result;
+        }
+    }
+}
